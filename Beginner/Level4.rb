0f9d82319726865169202af3a7c7c7e0d6715246
Mike Harris <mike.harris@groundsure.com>
2019-03-15 21:58:27 +0000

    Added Level 4

---
+++ Beginner/Level4.rb
@@ -0,0 +1,25 @@
+class Player
+  attr_reader :hero, :hearts
+  def play_turn(warrior)
+    @hero = warrior
+    @hearts ||= hero.health
+  	movement(warrior)
+    @hearts = hero.health
+  end
+  
+  def movement(warrior)
+    return hero.attack! if hero.feel.enemy?
+    return hero.walk! if under_fire?
+    return hero.rest! if injured?
+    hero.walk!
+  end
+
+  def injured?
+    hero.health < 20
+  end
+
+  def under_fire?
+    hero.health < hearts
+  end
+end
+  
