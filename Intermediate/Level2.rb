caf54be7ee4e4249575369e9c9d5ff04ee6cecc9
Mike Harris <mike.harris@groundsure.com>
2019-03-15 23:19:15 +0000

    Level 2 refactored

--- Intermediate/Level2.rb
+++ Intermediate/Level2.rb
@@ -1,8 +1,38 @@
 class Player
+	attr_reader :hero, :adjacent
 
   def play_turn(warrior)
-  	return warrior.attack!(warrior.direction_of_stairs) if warrior.feel(warrior.direction_of_stairs).enemy?
-    warrior.walk!(warrior.direction_of_stairs)
+  	@hero = warrior
+  	@adjacent = Adjacent.new(hero)
+  	return hero.attack!(adjacent.enemy_direction) if adjacent.enemy?
+  	return hero.rest! if injured?
+    hero.walk!(hero.direction_of_stairs)
+  end
+
+  def injured?
+  	hero.health < 20
+  end
+end
+
+class Adjacent
+	attr_reader :hero, :directions
+
+	def initialize(warrior)
+		@hero = warrior
+		@directions = {
+			forward: hero.feel(:forward),
+  		left: hero.feel(:left),
+  		right: hero.feel(:right),
+  		backward: hero.feel(:backward)
+  	}
+  end
+
+  def enemy?
+  	directions.any? { |dir, space| space.enemy? }
+  end
+
+  def enemy_direction
+  	directions.select { |dir, space| space.enemy? }.keys.first
   end
 
 end
