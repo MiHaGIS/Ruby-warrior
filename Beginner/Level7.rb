366e3a035021fbabf59a2ef313d81b6ab41e5b14
Mike Harris <mike.harris@groundsure.com>
2019-03-15 22:24:03 +0000

    Added Level 7

---
+++ Beginner/Level7.rb
@@ -0,0 +1,31 @@
+class Player
+  attr_reader :hero, :hearts
+  def play_turn(warrior)
+    @hero = warrior
+    @hearts ||= hero.health
+  	movement(warrior)
+    @hearts = hero.health
+  end
+  
+  def movement(warrior)
+    return hero.attack! if hero.feel.enemy?
+    return hero.rescue! if hero.feel.captive?
+    return hero.walk!(:backward) if near_death? && under_fire?
+    return hero.walk! if under_fire?
+    return hero.rest! if injured?
+    return hero.pivot! if hero.feel.wall?
+    hero.walk!
+  end
+
+  def injured?
+    hero.health < 20
+  end
+
+  def under_fire?
+    hero.health < hearts
+  end
+  
+  def near_death?
+    hero.health < 5
+  end
+end
